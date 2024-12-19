b0747acced7cda5c28955943ec266549cfb072aa
SbRivera <sbrivera2@espe.edu.ec>
2024-12-19 12:53:16 -0500

    gridview

---
+++ grid/lib/controller/coche_controller.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../models/coche.dart';
+
+class CocheController{
+  List<Coche> coches = [
+
+    Coche('Renault', 'Twingo', Image.asset('img/car1.png')),
+    Coche('Citroen', 'CS', Image.asset('img/car2.png')),
+    Coche('Ford', 'F150', Image.asset('img/car3.png')),
+    Coche('Porsche', 'Focus', Image.asset('img/car4.png')),
+    Coche('Renault', 'F340', Image.asset('img/car5.png')),
+    Coche('Renault', 'Twingo', Image.asset('img/car1.png')),
+    Coche('Citroen', 'CS', Image.asset('img/car2.png')),
+    Coche('Ford', 'F150', Image.asset('img/car3.png')),
+    Coche('Porsche', 'Focus', Image.asset('img/car4.png')),
+    Coche('Renault', 'F340', Image.asset('img/car5.png')),
+  ];
+}
